a60bb77b27d515c646fb22158bf035c93c53cd67
Puzanovim <puzanovim@yandex.ru>
2020-10-09 12:43:57 +0500

    first commit - add strategy pattern

---
+++ src/Observe/DisplayElement.java
@@ -0,0 +1,5 @@
+package Observe;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/Observe/WeatherStation.java
@@ -0,0 +1,14 @@
+package Observe;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        // Right here will be other displays
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
